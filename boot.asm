fd229753a9ce460c73a8a3a8c336a766f2f5f8f8
Hardik Roongta <hardikroongta8@gmail.com>
2025-10-19 12:59:11 +0530

    feat: add print function

--- boot.asm
+++ boot.asm
@@ -1,35 +1,16 @@
-mov ah, 0x0e
-mov al, 'H'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-int 0x10
-mov al, 'o'
-int 0x10
-mov al, ' '
-int 0x10
-mov al, 'W'
-int 0x10
-mov al, 'o'
-int 0x10
-mov al, 'r'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'd'
-int 0x10
-mov al, '!'
-int 0x10
-mov al, 0x0d
-int 0x10
-mov al, 0x0a
-int 0x10
+[org 0x7c00]
 
+HELLO:
+	db "Hello World!", 0
+
+mov bx, HELLO
+call print
+call println
 
 loop:
 	jmp loop
 
-times 510-($-$$) db 0
-dw 0xaa55 
+%include "print.asm"
+
+times 510 - ($-$$) db 0
+dw 0xaa55
