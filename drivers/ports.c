5af281fea18674f1ee593b1d23aa015146e6bc9c
Hardik Roongta <hardikroongta8@gmail.com>
2025-10-24 23:07:44 +0530

    refactor: remove unnecessary comments from ports.c

--- drivers/ports.c
+++ drivers/ports.c
@@ -1,41 +1,19 @@
 unsigned char port_read_byte(unsigned short port) {
   unsigned char result;
-  /**
-   * Put 'port' in 'edx' register
-   * Run the instruction
-   * Put the output in the 'eax' register
-   * Set 'result' to the value present in 'eax' register
-   */
   __asm__ volatile("inb %1, %0" : "=a"(result) : "Nd"(port) : "memory");
   return result;
 }
 
 void port_write_byte(unsigned short port, unsigned char data) {
-  /**
-   * Put 'port' in 'edx' register
-   * Put 'data' in 'eax' register
-   * Run the instruction
-   */
   __asm__ volatile("outb %1, %0" : : "Nd"(port), "a"(data) : "memory");
 }
 
 unsigned short port_read_word(unsigned short port) {
   unsigned short result;
-  /**
-   * Put 'port' in 'edx' register
-   * Run the instruction
-   * Put the output in the 'eax' register
-   * Set 'result' to the value present in 'eax' register
-   */
   __asm__ volatile("inw %1, %0" : "=a"(result) : "Nd"(port) : "memory");
   return result;
 }
 
 void port_write_word(unsigned short port, unsigned short data) {
-  /**
-   * Put 'port' in 'edx' register
-   * Put 'data' in 'eax' register
-   * Run the instruction
-   */
   __asm__ volatile("outw %1, %0" : : "Nd"(port), "a"(data) : "memory");
 }
